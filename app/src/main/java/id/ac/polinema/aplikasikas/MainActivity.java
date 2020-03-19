f11d1e0de776489e7b51369737fb99f4862df258
gaizkazaqhariou <gaizkapolinema@gmail.com>
2020-03-19 15:49:12 +0700

    fixing bug

--- app/src/main/java/id/ac/polinema/aplikasikas/MainActivity.java
+++ app/src/main/java/id/ac/polinema/aplikasikas/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity implements TransactionAdapte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
